c97ac63e80f85b2ba5db1e2fa33a12bd6c8c7524
ThomasMuij <thomasmuij@gmail.com>
2025-10-10 22:54:31 +0200

    updated gitignore

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify, render_template, redirect, url_for
 from markupsafe import escape
 from flask_cors import CORS
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app, origins="http://127.0.0.1:5500")
 
 @app.route('/')
-def hello():
+def say_hello():
     return 'hello'
 
 @app.route('/user/<username>')
@@ -65,11 +65,78 @@ def get_products():
 def login():
     if request.method == 'POST':
         # sys.stdout.clear()
-        print(request.content_type)
+        print(request.mimetype)
         print(request.get_data(as_text=True))
+        print(request.form)
+        print(request.data)
         return 'made a /login POST'
     else:
+        print(request.cookies)
         return 'made a /login GET'
+    
+@app.post('/login/post')
+def login_post():
+    return 'login successful'
+
+@app.route('/get/static')
+def get_static():
+    url = app.url_for('static', filename='style.css')
+    return url
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
+
+
+from flask import session
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/session')
+def session_index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+@app.route('/session/login', methods=['GET', 'POST'])
+def session_login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('session_index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/session/logout')
+def session_logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+from flask import Flask, flash
+
+@app.route('/flash')
+def flash_index():
+    return render_template('flash_index.html')
+
+@app.route('/flash/login', methods=['GET', 'POST'])
+def flash_login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'secret':
+            error = 'Invalid credentials'
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('flash_index'))
+    return render_template('flash_login.html', error=error)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
