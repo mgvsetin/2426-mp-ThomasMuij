119aa744922330ab7c8368d8a2161df4ca06f056
ThomasMuij <thomasmuij@gmail.com>
2025-10-12 21:22:03 +0200

    made db factory, db.py, schema.sql

---
+++ cashier_app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+import os
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABSE_CONNINFO = """
+            dbname=cashier_system
+            host=localhost
+            user=postgres
+            password=heslo123
+            port=5432"""
+    )
+
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    if test_config is None:
+        app.config.from_pyfile('config.py')
+    else:
+        app.config.from_mapping(test_config)
+
+    
+    @app.route('/temporary/config') # remove this
+    def temporary():
+        response = [str(k) for k in app.config.items()]
+        response.append(app.instance_path)
+        return response
+    
+    from . import db
+    db.init_app(app)
+
+    return app
---
+++ cashier_app/db.py
@@ -0,0 +1,41 @@
+from flask import g, current_app
+import click
+import psycopg
+from psycopg.rows import namedtuple_row
+
+
+def get_db(row_factory=namedtuple_row, **kwargs):
+    if 'db' not in g:
+        conninfo = current_app.config['DATABSE_CONNINFO']
+        
+        g.db = psycopg.connect(conninfo, row_factory=row_factory, **kwargs)
+        g
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    with current_app.open_resource('schema.sql', 'r', 'utf-8') as f:
+        sql = f.read()
+
+    with get_db(autocommit=True) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql)
+
+
+@click.command('init-db')
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
